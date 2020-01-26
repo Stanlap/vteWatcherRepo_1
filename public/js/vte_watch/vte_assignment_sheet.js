392dd1e39bd617b7d61b164be9ffb48521fac1df
stanlap <stanlap2@gmail.com>
2020-01-26 14:33:36 +0300

    work p3

--- public/js/vte_watch/vte_assignment_sheet.js
+++ public/js/vte_watch/vte_assignment_sheet.js
@@ -2,4 +2,39 @@ $(document).ready(function () {
     let objPatient = JSON.parse(localStorage.getItem('Patient'));
     // localStorage.clear();
     console.log(objPatient);
+    console.log(objPatient.pkMedProfile);
+    objPatient.pkMinTreatmentPeriod = 10;   
+    
+    switch (objPatient.pkChoosedDrugGroupLat) {
+
+        case 'Enoxaparin sodium':
+
+            break;
+
+        case 'Nadroparin calcium':
+            break;
+
+        case 'Heparin sodium':
+            break;
+
+        case 'Fondaparinux sodium':
+            break;
+
+        case 'Acetylsalicylic acid':
+            break;
+
+        case 'Dabigatran etexilate':
+            break;
+
+        case 'Rivaroxaban':
+            break;
+
+        case 'Apixaban':
+            break;
+
+        case 'Warfarin':
+            break;
+        default:
+
+    };
 });

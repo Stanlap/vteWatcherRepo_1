67422023824772cb122dc4e1274593547e37de7c
stanlap <stanlap2@gmail.com>
2020-02-07 16:01:29 +0300

    work on assignment sheet

--- public/js/vte_watch/vte_assignment_sheet.js
+++ public/js/vte_watch/vte_assignment_sheet.js
@@ -1,50 +1,51 @@
 $(document).ready(function () {
     let objPatient = JSON.parse(localStorage.getItem('Patient'));
-    // localStorage.clear();
-    console.log(objPatient);
-    // switch (objPatient.pkChoosedDrugGroupLat) {
-        let mTP = 0;
-    function defineMinTreatmentPeriod(mP, chosDrug) {
+    let objChoosedMedicines = JSON.parse(localStorage.getItem('ChoosedMedicines'));
+    // console.log(objPatient, objChoosedMedicines);
+
+    function defineMinTreatmentPeriod(mP, chosDrug, mTP) {
         switch (chosDrug) {
 
-            case 'Enoxaparin sodium':
+            case 'Эноксапарин натрия':
                 (mP === 1 || mP === 2) ? mTP = 6: (mP === 3 || mP === 4) ? mTP = 7 : '';
                 break;
 
-            case 'Nadroparin calcium':
+            case 'Надропарин кальция':
                 (mP < 4) ? mTP = 7: '';
                 break;
 
-            case 'Heparin sodium':
+            case 'Гепарин натрия':
                 mTP = 7;
                 break;
 
-            case 'Fondaparinux sodium':
+            case 'Фондапаринукс натрия':
                 (mP === 1 || mP === 2) ? mTP = 6: (mP === 3) ? mTP = 5 : '';
                 break;
 
-            case 'Dabigatran etexilate':
+            case 'Дабигатрана этексилат':
                 (mP === 4 && objPatient.pkArtroplastyKneeJoint === true) ? mTP = 28: '';
                 break;
 
-                // case 'Rivaroxaban':
-                //     break;
-
-            case 'Apixaban':
+            case 'Апиксабан':
                 (mP === 4 && objPatient.pkArtroplastyKneeJoint === true) ? mTP = 32: '';
                 break;
 
-            case 'Warfarin':
+            case 'Варфарин':
                 mTP = 4;
                 break;
-            default:
-                mTP = 10;
 
         };
         return mTP;
     }
 
-    objPatient.pkMinTreatmentPeriod = defineMinTreatmentPeriod(objPatient.pkMedProfile, objPatient.pkChoosedDrugGroupLat);
+    $(objChoosedMedicines).each(function(ind, el) {
+        el.treatPeriod = defineMinTreatmentPeriod(objPatient.pkMedProfile, el.titleGroupRu, 10);
+      });
+
+      console.log(objChoosedMedicines);
+      
+
+    objPatient.pkMinTreatmentPeriod = defineMinTreatmentPeriod(objPatient.pkMedProfile, objChoosedMedicines[0].titleGroupRu);
     console.log(objPatient.pkMedProfile);
     console.log(objPatient.pkMinTreatmentPeriod);
 

5a3f0b6b3d02c37a091c6bb5a7f4792a2a29f1ed
stanlap <stanlap2@gmail.com>
2020-02-15 18:25:21 +0300

    work on assign

--- public/js/vte_watch/vte_assignment_sheet.js
+++ public/js/vte_watch/vte_assignment_sheet.js
@@ -1,32 +1,208 @@
 $(document).ready(function () {
 
-    let objPatient = JSON.parse(localStorage.getItem('Patient'));
-    let objChoosedMedicines = JSON.parse(localStorage.getItem('ChoosedMedicines'));
+    let objPatient = JSON.parse(localStorage.getItem('Patient')),
+        objChoosedMedicines = JSON.parse(localStorage.getItem('ChoosedMedicines'));
+
     localStorage.removeItem('Patient');
     localStorage.removeItem('ChoosedMedicines');
 
     console.log(objPatient, objChoosedMedicines);
-    // console.log(objChoosedMedicines[1].startDateOfVTEProphyl);
 
-    $('<br><br>').appendTo('p');
     $('<div/>').attr({
-            id: 'invitToAct_1',
+            id: 'inviteToAct',
         })
         .html('Укажите дату начала профилактики ВТЭО:')
-        .appendTo('p');
+        .appendTo('#dialogMain');
+    $('<br>').appendTo('#dialogMain');
     $('<input/>').attr({
-            id: 'inpDate_1',
+            id: 'inpDate',
             type: 'date',
             value: formatDate()
         })
-        .appendTo('p');
-    $('<br><br>').appendTo('p');
+        .appendTo('#dialogMain');
+    $('<div>').attr({
+        id: 'dialog_2'
+    }).appendTo('#dialogMain');
+    $('<label/>').attr({
+        for: 'chkR_1'
+    }).html('Да').appendTo('#dialog_2');
+    $('<input/>').attr({
+        type: 'radio',
+        id: 'chkR_1',
+        name: 'chkRadio_1',
+        value: 0
+    }).appendTo('#dialog_2');
+    $('<label/>').attr({
+        for: 'chkR_2'
+    }).html('Нет').appendTo('#dialog_2');
+    $('<input/>').attr({
+        type: 'radio',
+        id: 'chkR_2',
+        name: 'chkRadio_1',
+        value: 1
+    }).appendTo('#dialog_2');
+    $('<br><br>').attr({
+        id: 'br_1'
+    }).appendTo('#dialogMain');
     $('<input/>').attr({
             id: 'btnOne',
             type: 'button',
             value: 'OK'
         })
-        .appendTo('p');
+        .appendTo('#dialogMain');
+
+    $('#dialogMain').hide();
+
+    let lineOfFuncs = [];
+
+    if($(objChoosedMedicines).length > 1){
+        lineOfFuncs.push(defineStartDateTakingOfSecMedicine);
+        const askOfBridgeTher = secDrug => 'Гепарин натрия' || secDrug === 'Эноксапарин натрия' || secDrug === 'Надропарин кальция' || secDrug === 'Бемипарин натрия' ? true : false;
+        askOfBridgeTher(objChoosedMedicines[1].titleGroupRu) ? lineOfFuncs.push(defineBridgeTherUsage) : '';    
+    }; 
+    function clearValues() {
+        $('#dialogMain').hide();
+        $('#inviteToAct').html('')
+        $('#inpDate').val('');
+        $('input[name = chkRadio_1]:checked').prop('checked', false);
+    }
+    function defineMinTreatmentPeriod(mP, chosDrug, mTP) {
+        switch (chosDrug) {
+            case 'Эноксапарин натрия':
+                (mP === 1 || mP === 2) ? mTP = 6: (mP === 3 || mP === 4) ? mTP = 7 : '';
+                break;
+            case 'Надропарин кальция':
+                (mP < 4) ? mTP = 7: '';
+                break;
+            case 'Гепарин натрия':
+                mTP = 7;
+                break;
+            case 'Фондапаринукс натрия':
+                (mP === 1 || mP === 2) ? mTP = 6: (mP === 3) ? mTP = 5 : '';
+                break;
+            case 'Дабигатрана этексилат':
+                (mP === 4 && objPatient.pkArtroplastyKneeJoint === true) ? mTP = 28: '';
+                break;
+            case 'Апиксабан':
+                (mP === 4 && objPatient.pkArtroplastyKneeJoint === true) ? mTP = 32: '';
+                break;
+            case 'Варфарин':
+                mTP = 4;
+                break;
+        };
+        return mTP;
+    }
+
+    $(objChoosedMedicines).each(function (ind, el) {
+        el.treatPeriod = defineMinTreatmentPeriod(objPatient.pkMedProfile, el.titleGroupRu, 10);
+        console.log(el.treatPeriod);
+    });
+
+    function initDialog() {
+        clearValues();
+        $('#dialogMain').show();
+        $('#dialog_2').hide();
+        $('#inviteToAct').html('Укажите дату начала профилактики ВТЭО:');
+        objPatient.pkIsOrNoSurg ? $('#inpDate').val(objPatient.pkDateOfOper) : $('#inpDate').val(formatDate());
+        $('#btnOne').bind('click', defineStartDateOfVTEProphyl);
+    }
+
+    function defineStartDateOfVTEProphyl() {
+        console.log(lineOfFuncs);
+        objPatient.pkstartDateOfVTEProphyl = $('#inpDate').val();
+        clearValues();
+        lineOfFuncs.length > 0 ? (lineOfFuncs[0](), lineOfFuncs.shift())  : $('#btnOne').unbind('click', defineStartDateOfVTEProphyl);
+        console.log(lineOfFuncs.length)
+    }
+
+    function defineStartDateTakingOfSecMedicine() {
+        $('#dialogMain').show();
+        $('#dialog_2').hide();
+        $('#inviteToAct').html('Укажите дату окончания профилактики ВТЭО первым препаратом:')
+        $('#inpDate').val(correctDate(addDays(new Date(), objChoosedMedicines[0].treatPeriod)));
+        $('#btnOne').bind('click', addStartDateOfSecondMedicine);
+    }
+
+    function addStartDateOfSecondMedicine() {
+        objChoosedMedicines[1].startDateOfVTEProphyl = $('#inpDate').val();
+        clearValues();
+        $('#btnOne').unbind('click', addStartDateOfSecondMedicine);
+    }
+
+
+    initDialog();
+
+
+    // $('#btnOne').on('click', function () {
+    //     objPatient.pkstartDateOfVTEProphyl = $('#inpDate').val();
+    //     $(objChoosedMedicines).length === 0 ? ($('#inviteToAct').html('Данные сохранены.'), $('#inpDate').val('').hide()) : '';
+    //     if ($(objChoosedMedicines).length === 1) {
+    //         objChoosedMedicines[0].startDateOfVTEProphyl = $('#inpDate').val();
+    //         $('#inviteToAct').html('Данные сохранены.');
+    //         $('#inpDate').val('').hide();
+    //     };
+    //     if ($(objChoosedMedicines).length > 1) {
+    //         $('#inviteToAct').html('Укажите дату окончания профилактики ВТЭО первым препаратом:')
+    //         let nextDateAfterTomorrow = addDays(new Date(), objChoosedMedicines[0].treatPeriod);
+    //         // console.log(nextDateAfterTomorrow);
+    //         $('#inpDate').val(`${nextDateAfterTomorrow.getFullYear()}-${('0' + (nextDateAfterTomorrow.getMonth() + 1)).slice(-2)}-${('0' + nextDateAfterTomorrow.getDate()).slice(-2)}`);
+    //         $('#btnOne').off();
+    //         $('#btnOne').bind('click', addStartDateOfSecondMedicine);
+    //     }
+    //     // $('#inpDate').val('').hide();         
+    // });
+
+
+
+
+
+    function defineBridgeTherUsage() {
+        $('#btnOne, #br_1').hide();
+        $('#inviteToAct').html('Планируется периоперационная мост-терапия НМГ или НФГ?');
+        $('<label/>').attr({
+            for: 'chkR_1'
+        }).html('Да').appendTo('p');
+        $('<input/>').attr({
+            type: 'radio',
+            id: 'chkR_1',
+            name: 'chkRadio_1',
+            value: 0
+        }).appendTo('p');
+        $('<label/>').attr({
+            for: 'chkR_2'
+        }).html('Нет').appendTo('p');
+        $('<input/>').attr({
+            type: 'radio',
+            id: 'chkR_2',
+            name: 'chkRadio_1',
+            value: 1
+        }).appendTo('p');
+        $('#btnOne').unbind('click', addStartDateOfSecondMedicine);
+        objPatient.pkIsOrNoSurg ? $('input[name = chkRadio_1]').bind('click', defineSpinalAnestUsage) : '';;
+    }
+
+    function defineSpinalAnestUsage() {
+        console.log($('input[name = chkRadio_1]:checked').val());
+        $('input[name = chkRadio_1]:checked').val() === '0' ? objPatient.pkBridgeTher = true : objPatient.pkBridgeTher = false;
+        console.log(objPatient.pkBridgeTher);
+        $('input[name = chkRadio_1]:checked').prop('checked', false);
+        $('#inviteToAct').html('Операция выполняется под спинномозговой анестезией?');
+        $('input[name = chkRadio_1]').unbind('click', defineSpinalAnestUsage)
+        // $('<label/>').attr({ for: 'chkR_1'}).html('Да').appendTo('p');    
+        // $('<input/>').attr({ type: 'radio', id: 'chkR_1', name: 'chkRadio_1', value: 0}).appendTo('p');
+        // $('<label/>').attr({ for: 'chkR_2'}).html('Нет').appendTo('p');
+        // $('<input/>').attr({ type: 'radio', id: 'chkR_2', name: 'chkRadio_1', value: 1}).appendTo('p');
+        // $('#btnOne').unbind('click', addStartDateOfSecondMedicine);
+
+        // $('#btnOne, #br_1').show();
+    }
+    // objPatient.pkIsOrNoSurg ? defineSpinalAnestUsage() : '';
+
+    // $('input[name = chkRadio_1]').bind('click', function(){
+    //     $(this).val() === 0 ? objPatient.pkSpinalAnest = true: objPatient.pkSpinalAnest = false;
+    //     console.log($(this).val());
+    //     console.log(objPatient.pkSpinalAnest);
+    // });
 
     function definePlateletsResearchTime(chosDrug, pRT = []) {
         // pRT[0]- исследование до начала профилактики ВТЭО, pRT[1] - первое исследование после начала профилактики ВТЭО, pRT[2] - интервал между исследованиями (сутки).
@@ -93,92 +269,7 @@ $(document).ready(function () {
         return allSamples;
     }
 
-    function defineMinTreatmentPeriod(mP, chosDrug, mTP) {
-        switch (chosDrug) {
 
-            case 'Эноксапарин натрия':
-                (mP === 1 || mP === 2) ? mTP = 6: (mP === 3 || mP === 4) ? mTP = 7 : '';
-                break;
-
-            case 'Надропарин кальция':
-                (mP < 4) ? mTP = 7: '';
-                break;
-
-            case 'Гепарин натрия':
-                mTP = 7;
-                break;
-
-            case 'Фондапаринукс натрия':
-                (mP === 1 || mP === 2) ? mTP = 6: (mP === 3) ? mTP = 5 : '';
-                break;
-
-            case 'Дабигатрана этексилат':
-                (mP === 4 && objPatient.pkArtroplastyKneeJoint === true) ? mTP = 28: '';
-                break;
-
-            case 'Апиксабан':
-                (mP === 4 && objPatient.pkArtroplastyKneeJoint === true) ? mTP = 32: '';
-                break;
-
-            case 'Варфарин':
-                mTP = 4;
-                break;
-
-        };
-        return mTP;
-    }
-
-    function formatDate() {
-        let d = new Date(),
-            month = '' + (d.getMonth() + 1),
-            day = '' + d.getDate(),
-            year = d.getFullYear(),
-            vDateNow = '';
-        if (month.length < 2) month = '0' + month;
-        if (day.length < 2) day = '0' + day;
-        vDateNow = [year, month, day].join('-');
-        return vDateNow;
-    }
-
-    function addDays(date, days) {
-        let result = new Date(date);
-        result.setDate(result.getDate() + days);
-        return result;
-    }
-
-    $('#btnOne').on('click', function () {
-        objPatient.pkstartDateOfVTEProphyl = $('#inpDate_1').val();
-        if ($(objChoosedMedicines).length > 0) {
-            objChoosedMedicines[0].startDateOfVTEProphyl = $('#inpDate_1').val();
-            console.log(objChoosedMedicines[0].startDateOfVTEProphyl);
-            $('#inpDate_1').val('');
-        };
-        if ($(objChoosedMedicines).length > 1) {
-
-            $('#invitToAct_1').html('Укажите дату начала профилактики ВТЭО вторым препаратом:')
-            $('#btnOne').off();
-            $('#btnOne').bind('click', addStartDateOfSecondMedicine);
-            let nextDateAfterTomorrow = addDays(new Date(), objChoosedMedicines[0].treatPeriod);
-            // console.log(nextDateAfterTomorrow);
-            $('#inpDate_1').val(`${nextDateAfterTomorrow.getFullYear()}-${('0' + (nextDateAfterTomorrow.getMonth() + 1)).slice(-2)}-${('0' + nextDateAfterTomorrow.getDate()).slice(-2)}`);
-        };
-    });
-
-    function addStartDateOfSecondMedicine() {
-        objChoosedMedicines[1].startDateOfVTEProphyl = $('#inpDate_1').val();
-        $('#btnOne').unbind('click', addStartDateOfSecondMedicine);
-        $('#inpDate_1').val('');
-        console.log(objChoosedMedicines[1].startDateOfVTEProphyl);
-        console.log(objChoosedMedicines);
-        console.log(objPatient.pkstartDateOfVTEProphyl);
-        $('#invitToAct_1').html('Данные успешно сохранены.')
-    }
-
-    if ($(objChoosedMedicines).length > 0) {
-        $(objChoosedMedicines).each(function (ind, el) {
-            el.treatPeriod = defineMinTreatmentPeriod(objPatient.pkMedProfile, el.titleGroupRu, 10);
-        });
-    };
 
     function defineFactorXaInhibitorsPerioperativeTactics(chosDrug, operBleedingRisk, CC, surgDifficulty, startDrugTakingAfterOper = 24) {
         let stopDrugTakingBeforeOper = 0;
@@ -206,7 +297,6 @@ $(document).ready(function () {
         return [stopDrugTakingBeforeOper, startDrugTakingAfterOper];
     }
 
-
     function changeFactorXaInhibitorsToVitaminKAntagonists(chosDrug_1, chosDrug_2, timeAlg = []) {
         if ((chosDrug_1 === 'Апиксабан' || chosDrug_1 === 'Ривароксабан' || chosDrug_1 === 'Дабигатрана этексилат') && chosDrug_2 === 'Варфарин') {
             // время начала приема ингибиторов фактора Ха в качестве второго лекарственного средства после антагонистов витамина К:
@@ -231,30 +321,57 @@ $(document).ready(function () {
         return timeAlg;
     }
 
-    function appointBridgeTherapy(chosDrug_1, chosDrug_2, bTHer,){
+    function appointBridgeTherapy(chosDrug_1, chosDrug_2, bTHer, ) {
         //  алгоритм вписать в справку позднее;
-        let med_1 = [], med_2 = [], vINR = [];
-        if(objPatient.pkIsOrNoOper, chosDrug_1 === 'Варфарин' && bTher){
+        let med_1 = [],
+            med_2 = [],
+            vINR = [];
+        if (objPatient.pkIsOrNoOper, chosDrug_1 === 'Варфарин' && bTher) {
             med_1[0] = -5;
-            med_1[1] = 1;  
-            vINR[0] = -1;        
-            vINR[1] = 0;        
-            vINR[2] = 1;        
+            med_1[1] = 1;
+            vINR[0] = -1;
+            vINR[1] = 0;
+            vINR[2] = 1;
 
-            if(chosDrug_2 === 'Гепарин натрия') {
+            if (chosDrug_2 === 'Гепарин натрия') {
                 med_2[0] = -2;
                 med_2[1] = -6;
                 med_2[3] = 3;
-objPatient.IsSmallOper ? med_2[2] = 12 : med_2[2] = 48;  
-                };
-            if(chosDrug_2 === 'Эноксапарин натрия' || chosDrug_2 === 'Надропарин кальция') {
+                objPatient.IsSmallOper ? med_2[2] = 12 : med_2[2] = 48;
+            };
+            if (chosDrug_2 === 'Эноксапарин натрия' || chosDrug_2 === 'Надропарин кальция') {
                 med_2[0] = -2;
                 med_2[1] = -24;
                 med_2[3] = 3;
-objPatient.IsSmallOper ? med_2[2] = 24 : med_2[2] = 48;  
+                objPatient.IsSmallOper ? med_2[2] = 24 : med_2[2] = 48;
             };
 
-        } 
+        }
     }
+
+    function stopVitaminKAntagonistsTakingBeforeOper(chosDrug, vINR, riskBleed) {
+        // Первое исследование МНО: vINRDate[0] даты до операции, vINRDate[1][0] -интервал, vINRDate[1][1] - длительность обследования в днях с данной частотой.
+        let vINRDate = [
+            [5, 1],
+            [1, 3]
+        ]
+        vDayStartTaking = 24,
+            vDayStopTaking = 2;
+
+        if (chosDrug = 'Варфарин') {
+            switch (vINR) {
+
+                case vINR > 4:
+                    vDayStopTaking = 5;
+                    break;
+                case vINR > 3 && vINR <= 4:
+                    vDayStopTaking = 3;
+                    break;
+            }
+        }
+
+    }
+
+    objPatient.pkOperBleedingRisk === 1 ? vDayStopTakingFactorXaInhibitors = 1 : vDayStopTakingFactorXaInhibitors = 2;
 
 });

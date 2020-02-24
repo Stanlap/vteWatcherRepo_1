279c36e87f9fecaab2f5fc995cc72e13574090cd
stanlap <stanlap2@gmail.com>
2020-02-24 11:03:15 +0300

    write test line func

--- public/scripts/date_manager.js
+++ public/scripts/date_manager.js
@@ -20,5 +20,33 @@ function correctDate(vD){
     return `${vD.getFullYear()}-${('0' + (vD.getMonth() + 1)).slice(-2)}-${('0' + vD.getDate()).slice(-2)}`;
 }
 
+// function plusOrMinusDays(startDate, vAddDays) {
+//     let searchedDate = startDate;
+//     searchedDate.setDate(searchedDate.getDate() + vAddDays);
+//     return searchedDate;
+// }
+
+// let day_1 = new Date(2019, 08, 20),
+//     day_2 = new Date(2019, 10, 07);
+
+// function diffDates(day_one, day_two) {
+//     return (day_one - day_two) / (60 * 60 * 24 * 1000);
+// };
+// diffDates(day_2, day_1);
+
+// let nextday = plusOrMinusDays(new Date("2020-02-19"), 7);
+// console.log(nextday);
+// let date1 = '02/19/2020';
+// let date2 = nextday.toLocaleDateString("en-US");
+// //   vplan_2 date1 = '01/12/2018';
+// // vplan_2 date2 = '12/12/2018';
+// date1 = new Date(date1);
+// date2 = new Date(date2);
+// date1 > date2; //false
+// date1 < date2; //true
+// date1 >= date2; //false
+// date1 <= date2; //true
+// console.log(date1 <= date2);
+
 
 

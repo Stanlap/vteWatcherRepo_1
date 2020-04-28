4b9dbe3bf09c091ed3fedf00146b0b5aa4ab7e73
stanlap <stanlap2@gmail.com>
2020-04-28 10:16:58 +0300

    add style to login form

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({
     extended: false
 }));
 app.use(express.static('public'));
-// app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + '/public'));
 app.use(
     session({
         secret: 'obobNMN23h',
@@ -40,13 +40,13 @@ app.set('view engine', 'ejs');
 // });
 
 app.get('/', (req, res) => {
-    res.render('main',{title: "Main page", message: "Main Page"});
+    res.render('main',{title: 'Main page'});
 });
 app.get('/login', (req, res) => {
-    res.render('login',{title: "Entry form", message: "Entry Page"});
+    res.render('login',{title: 'Entry form',  passMessage: 'Пароль допускает строчные и прописные латинские буквы, цифры, спецсимволы. Минимум 8 знаков.'});
 });
 app.get('/registration', (req, res) => {
-    res.render('regist',{title: "Registration form", message: "Registration Page"});
+    res.render('regist',{title: 'Registration form', message: 'Registration Page'});
 });
 app.post('/login', (req, res, next) => {
     passport.authenticate('local', function (err, user) {
@@ -54,13 +54,15 @@ app.post('/login', (req, res, next) => {
             return next(err);
         }
         if (!user) {
-            return res.send('Укажите правильный email или пароль!')
+            // return res.send('Укажите правильный email или пароль!');
+            return res.render('login',{title: 'Login',  passMessage: 'Пользователь не идентифицирован.'});
+
         }
         req.logIn(user, function (err) {
             if (err) {
                 return next(err);
             }
-            return res.render('programs',{title: "Programs Page", message: "Programs Page"});
+            return res.render('mainAuth',{title: 'Programs Page Authenticated'});
         });
     })(req, res, next);
 });
@@ -92,40 +94,40 @@ const auth = (req, res, next) => {
 }
 
 app.get('/profile', auth, (req, res) => {
-    res.render('profile',{title: "Profile", message: "Profile Page"});
+    res.render('profile',{title: 'Profile', message: 'Profile Page'});
 });
 app.get('/programs', auth, (req, res) => {
-    res.render('programs',{title: "Programs Page", message: "Programs Page"});
+    res.render('programs',{title: 'Programs Page', message: 'Programs Page'});
 });
 
 app.get('/vte_patient_profile', auth, (req, res) => {
-    res.render('vte_watch_ejs/vte_patient_profile',{title: "vte Watcher Patient Profile Page", message: "vte Watcher Patient Profile Page"});
+    res.render('vte_watch_ejs/vte_patient_profile',{title: 'vte Watcher Patient Profile Page', message: 'vte Watcher Patient Profile Page'});
 });
 
 app.get('/vte_patient_list_rf', auth, (req, res) => {
-    res.render('vte_watch_ejs/vte_patient_list_rf',{title: "vte Watcher Patient Risk Factors", message: "vte Watcher Patient Risk Factors"});
+    res.render('vte_watch_ejs/vte_patient_list_rf',{title: 'vte Watcher Patient Risk Factors', message: 'vte Watcher Patient Risk Factors'});
 });
 
 app.get('/vte_results_rf', auth, (req, res) => {
-    res.render('vte_watch_ejs/vte_results_rf',{title: "vte Watcher Patient Has Risk Factors", message: "vte Watcher Patient Has Risk Factors"});
+    res.render('vte_watch_ejs/vte_results_rf',{title: 'vte Watcher Patient Has Risk Factors', message: 'vte Watcher Patient Has Risk Factors'});
 });
 
 app.get('/vte_1', auth, (req, res) => {
-    res.render('vte_watch_ejs/vte_watch_1.ejs',{title: "vte Watcher Start Page", message: "vte Watcher Start Page"});
+    res.render('vte_watch_ejs/vte_watch_1.ejs',{title: 'vte Watcher Start Page', message: 'vte Watcher Start Page'});
 });
 
 app.get('/vte_2', auth, (req, res) => {
-    res.render('vte_watch_ejs/vte_watch_2.ejs',{title: "vte Watcher Second Page", message: "vte Watcher Second Page"});
+    res.render('vte_watch_ejs/vte_watch_2.ejs',{title: 'vte Watcher Second Page', message: 'vte Watcher Second Page'});
 });
 app.get('/vte_concl', auth, (req, res) => {
-    res.render('vte_watch_ejs/vte_concl.ejs',{title: "vte Conclusion about risk of VTE", message: "vte Conclusion about risk of VTE"});
+    res.render('vte_watch_ejs/vte_concl.ejs',{title: 'vte Conclusion about risk of VTE', message: 'vte Conclusion about risk of VTE'});
 });
 app.get('/vte_drug', auth, (req, res) => {
-    res.render('vte_watch_ejs/vte_drug.ejs',{title: "vte Define Drug", message: "vte Define Drug"});
+    res.render('vte_watch_ejs/vte_drug.ejs',{title: 'vte Define Drug', message: 'vte Define Drug'});
 });
 
 app.get('/vte_assignment_sheet', auth, (req, res) => {
-    res.render('vte_watch_ejs/vte_assignment_sheet.ejs',{title: "vte Assignment sheet", message: "vte Assignment sheet"});
+    res.render('vte_watch_ejs/vte_assignment_sheet.ejs',{title: 'vte Assignment sheet', message: 'vte Assignment sheet'});
 });
 
 app.post('/count', auth, (req, res) => {
---
+++ public/js/login.js
@@ -0,0 +1,3 @@
+    $('#spnPasswHelp').html() === 'Пользователь не идентифицирован.' ? $('#spnPasswHelp').addClass('text-info') : $('#spnPasswHelp').removeClass('text-info');
+
+

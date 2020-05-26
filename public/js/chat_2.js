6850d79373037ff4e5516ed298f0ee80beb5cac0
stanlap <stanlap2@gmail.com>
2020-05-26 17:52:18 +0300

    make chat

--- public/js/chat_2.js
+++ public/js/chat_2.js
@@ -1,30 +1,23 @@
 
-const ws = new WebSocket('ws://localhost:3000');
-const messages = $('#ulMsgs');
-const status = $('#pStatus');
-
-function printMessage(value) {
-    $('<li/>'). prop({class: 'list-group-item border-0'}).html(value).appendTo(messages);
-}
-
-const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
-function setStatus(value) {
-    status.html(value);
-}
+const ws = new WebSocket('ws://localhost:3000'),
+messages = $('#ulMsgs'),
+status = $('#pStatus');
+let vName ='';
 
 $('#frmMsg').on('submit', event => {
     event.preventDefault();
-
-    let vMsg = `<div class="divMsg bg-${getRandomInt(2) ? 'secondary': 'info'} rounded w-75 float-${getRandomInt(2) ? 'right': 'left'} mb-2"><div class="p-2"><p class="card-text text-white"><strong>${$('#inpName').val()}</strong> ${$('#taMsg').val()}</p></div></div>`;
-    ws.send(vMsg);
-
-//     $(`.divMsg :contains(${vName})`).addClass('bg-primary');
-//     $('.divMsg').each((ind, el) => {
-//      $(el).addClass(`${getRandomInt(2) ? 'bg-secondary': 'bg-info'};`)   
-//     })
-// $('#frmMsg').before(vMsg);
+    vName = $('#inpName').val();
+    let vContent = $('#taMsg').val();
+    ws.send(`<strong>${vName}:</strong>   ${vContent}`);
+    $('#taMsg').val('');
 });
 
+const setStatus = value => status.html(value);
 ws.onopen = ()=> setStatus('ONLINE');
 ws.onclose = ()=> setStatus('DISCONNECTED');
+
+const printMessage = value => {
+    $('<li/>').prop({class: 'list-group-item border-0'}).html(value).appendTo(messages);
+    vName ? $(`li :contains(${vName})`).addClass('text-primary'): '';
+}
 ws.onmessage = response => printMessage(response.data);
